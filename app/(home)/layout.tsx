547ce26f8c2b330be899fbc73c53120bed010f95
mihiragath <mihiragath@users.noreply.github.com>
2025-02-28 23:45:08 +0530

    add

--- app/(home)/layout.tsx
+++ app/(home)/layout.tsx
@@ -1,6 +1,6 @@
-import { Navbar } from "@/components/home/header/navbar";
-import { prisma } from "@/lib/prisma";
-import { currentUser } from "@clerk/nextjs/server";
+  import { Navbar } from "@/components/home/header/navbar";
+  import { prisma } from "@/lib/prisma";
+  import { currentUser } from "@clerk/nextjs/server";
 import React from "react";
 
 const layout = async ({ children }: { children: React.ReactNode }) => {
